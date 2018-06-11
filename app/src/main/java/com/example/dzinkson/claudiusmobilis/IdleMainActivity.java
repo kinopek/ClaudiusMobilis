d41c2b2df0f6b7c511243fa611878aef43551e5d
dzinkson <dzinkson@users.noreply.github.com>
2018-06-11 16:54:05 +0200

    estetyka

--- app/src/main/java/com/example/dzinkson/claudiusmobilis/IdleMainActivity.java
+++ app/src/main/java/com/example/dzinkson/claudiusmobilis/IdleMainActivity.java
@@ -3,11 +3,13 @@ package com.example.dzinkson.claudiusmobilis;
 import android.app.Activity;
 //import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class IdleMainActivity extends Activity {
 
@@ -43,5 +45,8 @@ public class IdleMainActivity extends Activity {
                 startActivity(startIntent);
             }
         });
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/dzinkson/claudiusmobilis/State.java
@@ -0,0 +1,4 @@
+package com.example.dzinkson.claudiusmobilis;
+
+public class State {
+}
